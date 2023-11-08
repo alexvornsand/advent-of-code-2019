559d667b65b1384e758d4d81ad17dea276e1a5d0
alexvornsand <29985111+alexvornsand@users.noreply.github.com>
2023-11-08 15:44:39 -0600

    update day 2

--- solutions/day-02/day-02.py
+++ solutions/day-02/day-02.py
@@ -1,38 +1,68 @@
 # advent of code 2019
 # day 2
 
-# part 1
-program = tuple([int(x) for x in open('input.txt', 'r').read().split(',')])
+file = 'input.txt'
 
-def runProgram(program, noun, verb):
-    programMemory = list(program)
-    programMemory[1] = noun
-    programMemory[2] = verb
-    i = 0
-    while(True):
-        if programMemory[i] == 99:
-            break
+class Intcode:
+    def __init__(self, code): 
+        self.script = tuple(code)
+        self.program = list(self.script)
+        self.directive = 'continue'
+        self.pointer = 0
+    
+    def resetComputer(self):
+        self.program = list(self.script)
+        self.directive = 'continue'
+        self.pointer = 0
+
+    def runProgram(self):
+        while(self.directive == 'continue'):
+            self.evaluateStep()
+
+    def evaluateStep(self):
+        code = self.program[self.pointer]
+        if code == 1:
+            self.add(self.pointer + 1, self.pointer + 2, self.pointer + 3)
+            self.directive = 'continue'
+            self.pointer += 4
+        elif code == 2:
+            self.multiply(self.pointer + 1, self.pointer + 2, self.pointer + 3)
+            self.directive = 'continue'
+            self.pointer += 4
+        elif code == 99:
+            self.directive = 'break'
+            self.pointer += 1
         else:
-            if programMemory[i] == 1:
-                programMemory[programMemory[i + 3]] = programMemory[programMemory[i + 1]] + programMemory[programMemory[i + 2]]
-                i += 4
-            elif programMemory[i] == 2:
-                programMemory[programMemory[i + 3]] = programMemory[programMemory[i + 1]] * programMemory[programMemory[i + 2]]
-                i += 4
-            else:
+            self.directive = 'damaged'
+
+    def add(self, pointerA, pointerB, pointerC):
+        self.program[self.program[pointerC]] = self.program[self.program[pointerA]] + self.program[self.program[pointerB]]
+
+    def multiply(self, pointerA, pointerB, pointerC):
+        self.program[self.program[pointerC]] = self.program[self.program[pointerA]] * self.program[self.program[pointerB]]
+
+def part_1(intcode):
+    intcode.program[1] = 12
+    intcode.program[2] = 2
+    intcode.runProgram()
+    print('Part 1:', intcode.program[0])
+
+def part_2(intcode):
+    for noun in range(100):
+        for verb in range(100):
+            intcode.resetComputer()
+            intcode.program[1] = noun
+            intcode.program[2] = verb
+            intcode.runProgram()
+            if intcode.program[0] == 19690720:
+                print('Part 2:', str(100 * noun + verb))
                 break
-    return(programMemory[0])
+            
+def main():
+    code = [int(x) for x in open('input.txt', 'r').read().split(',')]
+    intcode = Intcode(code)
+    part_1(intcode)
+    part_2(intcode)
 
-def buildComputer(program, partTwo=False):
-    if partTwo is False:
-        return(runProgram(program, 12, 2))
-    else:
-        for n in range(100):
-            for v in range(100):
-                if runProgram(program, n, v) == 19690720:
-                    return(100 * n + v)
-
-buildComputer(program)
-
-# part 2
-buildComputer(program, True)
+if __name__ == '__main__':
+    main()
