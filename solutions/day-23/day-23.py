678c391033942944cf188a9867ef467cabc7e358
alexvornsand <29985111+alexvornsand@users.noreply.github.com>
2023-11-29 19:17:39 -0600

    Create day-23.py

---
+++ solutions/day-23/day-23.py
@@ -0,0 +1,312 @@
+# advent of code 2019
+# day 23
+
+file = 'input.txt'
+
+class Intcode:
+    def __init__(self, code): 
+        self.script = tuple(code)
+        self.program = {i: self.script[i] for i in range(len(self.script))}
+        self.directive = 'continue'
+        self.pointer = 0
+        self.relative_base = 0
+        self.output_values = []
+        self.input_values = []
+        self.verbose = False
+    
+    def resetComputer(self):
+        self.program = {i: self.script[i] for i in range(len(self.script))}
+        self.directive = 'continue'
+        self.pointer = 0
+        self.relative_base = 0
+        self.output_values = []
+        self.input_values = []
+        self.verbose = False
+    
+    def clearOutput(self):
+        self.output_values = []
+
+    def printProgram(self):
+        return [str(key) + ': ' + str(self.program[key]) for key in sorted(list(self.program.keys()))]
+
+    def runProgram(self, input=[]):
+        self.directive = 'continue'
+        self.input_values = input
+        while(self.directive == 'continue'):
+            self.evaluateStep()
+
+    def identifyValue(self, parameter):
+        address, parameter_code = parameter
+        if parameter_code == 0:
+            if self.program[address] not in self.program:
+                self.program[self.program[address]] = 0
+            return self.program[address]
+        elif parameter_code == 1:
+            if address not in self.program:
+                address
+            return address
+        elif parameter_code == 2:
+            if self.relative_base + self.program[address] not in self.program:
+                self.program[self.relative_base + self.program[address]] = 0
+            return self.relative_base + self.program[address]
+
+    def evaluateStep(self):
+        instruction = str(self.program[self.pointer]).zfill(5)
+        opcode = int(instruction[-2:])
+        parameter_codes = [int(param) for param in instruction[:-2][::-1]]
+        if self.verbose:
+            print('pointer:', self.pointer)
+            print('instruction:', str(self.program[self.pointer]))
+        if opcode == 1:
+            parameters = list(zip([self.pointer + 1, self.pointer + 2, self.pointer + 3], parameter_codes))
+            self.add(parameters)
+        elif opcode == 2:
+            parameters = list(zip([self.pointer + 1, self.pointer + 2, self.pointer + 3], parameter_codes))
+            self.multiply(parameters)
+        elif opcode == 3:
+            parameters = list(zip([self.pointer + 1], parameter_codes))
+            self.input(parameters)
+        elif opcode == 4:
+            parameters = list(zip([self.pointer + 1], parameter_codes))
+            self.output(parameters)
+        elif opcode == 5:
+            parameters = list(zip([self.pointer + 1, self.pointer + 2], parameter_codes))
+            self.jumpIfTrue(parameters)
+        elif opcode == 6:
+            parameters = list(zip([self.pointer + 1, self.pointer + 2], parameter_codes))
+            self.jumpIfFalse(parameters)
+        elif opcode == 7:
+            parameters = list(zip([self.pointer + 1, self.pointer + 2, self.pointer + 3], parameter_codes))
+            self.lessThan(parameters)
+        elif opcode == 8:
+            parameters = list(zip([self.pointer + 1, self.pointer + 2, self.pointer + 3], parameter_codes))
+            self.equals(parameters)
+        elif opcode == 9:
+            parameters = list(zip([self.pointer + 1], parameter_codes))
+            self.adjustRelativeBase(parameters)
+        elif opcode == 99:
+            self.directive = 'complete'
+            self.pointer += 1
+        else:
+            self.directive = 'damaged'
+
+    def add(self, parameters):
+        if self.verbose:
+            print('\tadd:')
+            print('\t...', str(self.program[self.pointer]), str(self.program[self.pointer + 1]), str(self.program[self.pointer + 2]), str(self.program[self.pointer + 3]), '...', sep=', ')
+            print('\t', str(parameters[2]), ' = ', str(parameters[0]), ' + ', str(parameters[1]), sep='')
+            print('\t[', str(self.identifyValue(parameters[2])), '] = ', str(self.program[self.identifyValue(parameters[0])]), ' + ', str(self.program[self.identifyValue(parameters[1])]), sep='')
+        self.program[self.identifyValue(parameters[2])] = self.program[self.identifyValue(parameters[0])] + self.program[self.identifyValue(parameters[1])]
+        if self.verbose:
+            print('\t[', str(self.identifyValue(parameters[2])), '] = ', str(self.program[self.identifyValue(parameters[2])]), sep='')
+        self.directive = 'continue'
+        self.pointer += 4
+
+    def multiply(self, parameters):
+        if self.verbose:
+            print('\tmultiply:')
+            print('\t...', str(self.program[self.pointer]), str(self.program[self.pointer + 1]), str(self.program[self.pointer + 2]), str(self.program[self.pointer + 3]), '...', sep=', ')
+            print('\t', str(parameters[2]), ' = ', str(parameters[0]), ' * ', str(parameters[1]), sep='')
+            print('\t[', str(self.identifyValue(parameters[2])), '] = ', str(self.program[self.identifyValue(parameters[0])]), ' * ', str(self.program[self.identifyValue(parameters[1])]), sep='')
+        self.program[self.identifyValue(parameters[2])] = self.program[self.identifyValue(parameters[0])] * self.program[self.identifyValue(parameters[1])]
+        if self.verbose:
+            print('\t[', str(self.identifyValue(parameters[2])), '] = ', str(self.program[self.identifyValue(parameters[2])]), sep='')
+        self.directive = 'continue'
+        self.pointer += 4
+
+    def input(self, parameters):
+        if len(self.input_values) == 0:
+            if self.verbose:
+                print('\tinput:')
+                print('\t...', str(self.program[self.pointer]), str(self.program[self.pointer + 1]), '...', sep=', ')
+                print('\tno input')
+            self.directive = 'pause'
+        else:
+            if self.verbose:
+                print('\tinput:')
+                print('\t...', str(self.program[self.pointer]), str(self.program[self.pointer + 1]), '...', sep=', ')
+                print('\t', str(parameters[0]), ' = ', str(self.input_values[0]), sep='')
+                print('\t[', str(self.identifyValue(parameters[0])), '] = ', str(self.input_values[0]), sep='')
+            self.program[self.identifyValue(parameters[0])] = self.input_values[0]
+            if self.verbose:
+                print('\t[', str(self.identifyValue(parameters[0])), '] = ', str(self.program[self.identifyValue(parameters[0])]), sep='')
+            self.input_values.pop(0)
+            self.directive = 'continue'
+            self.pointer += 2
+
+    def output(self, parameters):
+        if self.verbose:
+            print('\toutput:')
+            print('\t...', str(self.program[self.pointer]), str(self.program[self.pointer + 1]), '...', sep=', ')
+            print('\t', str(self.program[self.identifyValue(parameters[0])]), sep='')
+        self.output_values.append(self.program[self.identifyValue(parameters[0])])
+        if self.verbose:
+            print('\toutputs:', self.output_values)
+        self.directive = 'continue'
+        self.pointer += 2
+
+    def jumpIfTrue(self, parameters):
+        if self.program[self.identifyValue(parameters[0])] != 0:
+            if self.verbose:
+                print('\tjump if true:')
+                print('\t...', str(self.program[self.pointer]), str(self.program[self.pointer + 1]), str(self.program[self.pointer + 2]), '...', sep=', ')
+                print('\t', str(self.program[self.identifyValue(parameters[0])]), ' != 0', sep='')
+                print('\t\tpointer =', str(self.program[self.identifyValue(parameters[1])]))
+            self.directive = 'continue'
+            self.pointer = self.program[self.identifyValue(parameters[1])]
+            if self.verbose:
+                print('\tpointer:', str(self.pointer))
+        else:
+            if self.verbose:
+                print('\tjump if true:')
+                print('\t...', str(self.program[self.pointer]), str(self.program[self.pointer + 1]), str(self.program[self.pointer + 2]), '...', sep=', ')
+                print('\t', str(self.program[self.identifyValue(parameters[0])]), ' != 0', sep='')
+                print('\t\tpointer =', str(self.pointer + 3))
+            self.directive = 'continue'
+            self.pointer += 3
+            if self.verbose:
+                print('\tpointer:', str(self.pointer))
+
+    def jumpIfFalse(self, parameters):
+        if self.program[self.identifyValue(parameters[0])] == 0:
+            if self.verbose:
+                print('\tjump if false:')
+                print('\t...', str(self.program[self.pointer]), str(self.program[self.pointer + 1]), str(self.program[self.pointer + 2]), '...', sep=', ')
+                print('\t', str(self.program[self.identifyValue(parameters[0])]), ' == 0', sep='')
+                print('\t\tpointer ->', str(self.program[self.identifyValue(parameters[1])]))
+            self.directive = 'continue'
+            self.pointer = self.program[self.identifyValue(parameters[1])]
+            if self.verbose:
+                print('\tpointer:', str(self.pointer))
+        else:
+            if self.verbose:
+                print('\tjump if false:')
+                print('\t...', str(self.program[self.pointer]), str(self.program[self.pointer + 1]), str(self.program[self.pointer + 2]), '...', sep=', ')
+                print('\t', str(self.program[self.identifyValue(parameters[0])]), ' == 0', sep='')
+                print('\t\tpointer ->', self.pointer)
+            self.directive = 'continue'
+            self.pointer += 3
+            if self.verbose:
+                print('\tpointer:', str(self.pointer))
+
+    def lessThan(self, parameters):
+        if self.program[self.identifyValue(parameters[0])] < self.program[self.identifyValue(parameters[1])]:
+            if self.verbose:
+                print('\tless than:')
+                print('\t...', str(self.program[self.pointer]), str(self.program[self.pointer + 1]), str(self.program[self.pointer + 2]), str(self.program[self.pointer + 3]), '...', sep=', ')
+                print('\t', str(self.program[self.identifyValue(parameters[0])]), ' < ', str(self.program[self.identifyValue(parameters[1])]), sep='')
+                print('\t\t[', str(self.identifyValue(parameters[2])), '] = 1', sep='')
+            self.directive = 'continue'
+            self.program[self.identifyValue(parameters[2])] = 1
+            if self.verbose:
+                print('\t[', str(self.identifyValue(parameters[2])), '] = ', str(self.program[self.identifyValue(parameters[2])]), sep='')
+        else:
+            if self.verbose:
+                print('\tless than:')
+                print('\t...', str(self.program[self.pointer]), str(self.program[self.pointer + 1]), str(self.program[self.pointer + 2]), str(self.program[self.pointer + 3]), '...', sep=', ')
+                print('\t', str(self.program[self.identifyValue(parameters[0])]), ' < ', str(self.program[self.identifyValue(parameters[1])]), sep='')
+                print('\t\t[', str(self.identifyValue(parameters[2])), '] = 0', sep='')
+            self.directive = 'continue'
+            self.program[self.identifyValue(parameters[2])] = 0
+            if self.verbose:
+                print('\t[', str(self.identifyValue(parameters[2])), '] = ', str(self.program[self.identifyValue(parameters[2])]), sep='')
+        self.pointer += 4
+
+    def equals(self, parameters):
+        if self.program[self.identifyValue(parameters[0])] == self.program[self.identifyValue(parameters[1])]:
+            if self.verbose:
+                print('\tequal:')
+                print('\t...', str(self.program[self.pointer]), str(self.program[self.pointer + 1]), str(self.program[self.pointer + 2]), str(self.program[self.pointer + 3]), '...', sep=', ')
+                print('\t', str(self.program[self.identifyValue(parameters[0])]), ' == ', str(self.program[self.identifyValue(parameters[1])]), sep='')
+                print('\t\t[', str(self.identifyValue(parameters[2])), '] = 1', sep='')
+            self.directive = 'continue'
+            self.program[self.identifyValue(parameters[2])] = 1
+            if self.verbose:
+                print('\t[', str(self.identifyValue(parameters[2])), '] = ', str(self.program[self.identifyValue(parameters[2])]), sep='')
+        else:
+            if self.verbose:
+                print('\tequal:')
+                print('\t...', str(self.program[self.pointer]), str(self.program[self.pointer + 1]), str(self.program[self.pointer + 2]), str(self.program[self.pointer + 3]), '...', sep=', ')
+                print('\t', str(self.program[self.identifyValue(parameters[0])]), ' == ', str(self.program[self.identifyValue(parameters[1])]), sep='')
+                print('\t\t[', str(self.identifyValue(parameters[2])), '] = 0', sep='')
+            self.directive = 'continue'
+            self.program[self.identifyValue(parameters[2])] = 0
+            if self.verbose:
+                print('\t[', str(self.identifyValue(parameters[2])), '] = ', str(self.program[self.identifyValue(parameters[2])]), sep='')
+        self.pointer += 4
+
+    def adjustRelativeBase(self, parameters):
+        if self.verbose:
+            print('\tadjust relative base:')
+            print('\t...', str(self.program[self.pointer]), str(self.program[self.pointer + 1]), '...', sep=', ')
+            print('\t', str(self.relative_base), ' + ', str(self.program[self.identifyValue(parameters[0])]), sep='')
+        self.relative_base += self.program[self.identifyValue(parameters[0])]
+        if self.verbose:
+            print('\trelative base:', str(self.relative_base))
+        self.pointer += 2
+
+def part_1(computers):
+    def buildNetwork(computers):
+        inputs = {i: [] for i in range(50)}
+        for i in computers:
+            computers[i].runProgram([i])
+        while True:
+            for i in computers:
+                computers[i].output_values = []
+                if len(inputs[i]) == 0:
+                    computers[i].runProgram([-1])
+                else:
+                    computers[i].runProgram(inputs[i])
+                    inputs[i] = []
+                instructions = computers[i].output_values.copy()
+                while len(instructions) > 0:
+                    address, x, y = instructions[:3]
+                    if address == 255:
+                        return y
+                    instructions = instructions[3:]
+                    inputs[address] += [x, y]
+    print('Part 1:', buildNetwork(computers))
+
+def part_2(computers):
+    def buildNetwork(computers):
+        for i in computers:
+            computers[i].resetComputer()
+        inputs = {i: [] for i in range(50)}
+        NAT = []
+        current_handoff = []
+        last_handoff = []
+        for i in computers:
+            computers[i].runProgram([i])
+        while True:
+            if all([val == [] for val in inputs.values()]):
+                if NAT != []:
+                    inputs[0] = NAT
+                    last_handoff = current_handoff.copy()
+                    current_handoff = NAT.copy()
+                    if current_handoff == last_handoff:
+                        return current_handoff[1]
+            for i in computers:
+                computers[i].output_values = []
+                if len(inputs[i]) == 0:
+                    computers[i].runProgram([-1])
+                else:
+                    computers[i].runProgram(inputs[i])
+                    inputs[i] = []
+                instructions = computers[i].output_values.copy()
+                while len(instructions) > 0:
+                    address, x, y = instructions[:3]
+                    instructions = instructions[3:]
+                    if address == 255:
+                        NAT = [x, y]
+                    else:
+                        inputs[address] += [x, y]
+    print('Part 2:', buildNetwork(computers))
+
+def main():
+    code = [int(x) for x in open(file, 'r').read().split(',')]
+    computers = {i: Intcode(code) for i in range(50)}
+    part_1(computers)
+    part_2(computers)
+
+if __name__ == '__main__':
+    main()
